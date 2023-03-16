2b707b0d7b9cfd3c25e422300c57f782e9129c3c
Henrique de Paula <henrique.depaula@hotmail.com>
2023-03-16 15:39:11 -0300

    feat (PAUTA): adicionado dto de response da Pauta

---
+++ src/main/java/com/sicredi/desafiovotacao/api/controller/v1/topic/dto/TopicResponse.java
@@ -0,0 +1,30 @@
+package com.sicredi.desafiovotacao.api.controller.v1.topic.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TopicResponse {
+
+    @JsonProperty(value = "pauta_id")
+    private String topicId;
+
+    @JsonProperty(value = "data_criacao")
+    private LocalDateTime creationDate;
+
+    private String message;
+
+    public static TopicResponse of(String id, LocalDateTime creationDate, String message) {
+        return new TopicResponse(id, creationDate, message);
+    }
+
+    public static TopicResponse createEmptyResponse() {
+        return new TopicResponse();
+    }
+}
