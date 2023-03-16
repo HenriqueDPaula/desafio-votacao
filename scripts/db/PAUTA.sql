6112c636aabdd89fc6944e3f713591e750c6ac09
Henrique de Paula <henrique.depaula@hotmail.com>
2023-03-16 15:29:25 -0300

    feat: adicionado DDL da tabela PAUTA

---
+++ scripts/db/PAUTA.sql
@@ -0,0 +1,7 @@
+CREATE TABLE PAUTA (
+    ID                       VARCHAR NOT NULL,
+    ASSUNTO                  VARCHAR NOT NULL,
+    DATA_CRIACAO             TIMESTAMP     NOT NULL,
+
+CONSTRAINT PK_PAUTA          PRIMARY KEY (ID)
+);
