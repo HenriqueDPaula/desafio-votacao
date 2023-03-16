1460bbb5d3db7cb26b48fd4d15e3eb86b7e068a1
Henrique de Paula <henrique.depaula@hotmail.com>
2023-03-16 15:47:47 -0300

    feat (PAUTA): adicionado service na camada de controller

--- src/main/java/com/sicredi/desafiovotacao/api/controller/v1/topic/TopicController.java
+++ src/main/java/com/sicredi/desafiovotacao/api/controller/v1/topic/TopicController.java
@@ -18,13 +18,24 @@ import static org.springframework.http.HttpStatus.CREATED;
 @RestController
 @RequestMapping("/v1/pauta")
 public class TopicController {
+
+    private final TopicService topicService;
+
     private static final String TOPIC_CREATE_MESSAGE = "Pauta %s criado com sucesso!";
+    private static final String EMPTY = "";
+
+    @Autowired
+    public TopicController(TopicService topicService) {
+        this.topicService = topicService;
+    }
 
     @PostMapping
     public ResponseEntity<TopicResponse> create(@RequestBody @Valid TopicRequest topicRequest) {
 
+        String id = topicService.createTopic(topicRequest).orElse(EMPTY);
+
         return new ResponseEntity<>(
-                TopicResponse.of("id", topicRequest.getCreationDate(),String.format(TOPIC_CREATE_MESSAGE, topicRequest.getSubject())),
+                TopicResponse.of(id, topicRequest.getCreationDate(),String.format(TOPIC_CREATE_MESSAGE, topicRequest.getSubject())),
                 CREATED);
 
     }
