b754f78178b130c1129d920debcad404ef24dca1
Henrique de Paula <henrique.depaula@hotmail.com>
2023-03-17 18:45:19 -0300

    feat(ASSOCIADO): Repositorio

---
+++ src/main/java/com/sicredi/desafiovotacao/usecase/associate/repository/AssociateRepository.java
@@ -0,0 +1,16 @@
+package com.sicredi.desafiovotacao.usecase.associate.repository;
+
+import com.sicredi.desafiovotacao.entity.AssociateTable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AssociateRepository extends CrudRepository<AssociateTable, String> {
+
+    Optional<AssociateTable> findByCpf(String cpf);
+
+    Optional<AssociateTable> findByIdAndSessionId(String id, String sessionId);
+
+}
