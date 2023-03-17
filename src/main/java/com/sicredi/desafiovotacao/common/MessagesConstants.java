eb6af3d536f0862953bfd1b6a175cd7f3e16f792
Henrique de Paula <henrique.depaula@hotmail.com>
2023-03-17 18:51:36 -0300

    feat(ASSOCIADO): Constantes de mensagens adicionadas

--- src/main/java/com/sicredi/desafiovotacao/common/MessagesConstants.java
+++ src/main/java/com/sicredi/desafiovotacao/common/MessagesConstants.java
@@ -3,8 +3,12 @@ package com.sicredi.desafiovotacao.common;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class MessagesConstants {
+
+    private static final List<String> voteOptions = List.of("SIM", "NÃO", "sim", "não", "s", "n");
 
     public static final String INVALID_DATE_BETWEEN = "inicio_sessao deve ser inferior a fim_sessao";
     public static final String INVALID_DATE_START = "inicio_sessao deve ser superior a data atual";
@@ -13,7 +17,9 @@ public final class MessagesConstants {
     public static final String CLOSED_SESSION_ERROR = "Sessão {%s} encontra-se encerrada";
     public static final String UNIQUE_VOTE_CONSTRAINT = "Associado já possui voto registrado para a sessão {%s}";
 
-    public static final String SESSION_CREATE_MESSAGE = "Sessão criada com sucesso!";
+    public static final String INVALID_VOTE_OPTION = String.format("Voto deve ser contabilizado como: %s", voteOptions);
 
+    public static final String SESSION_CREATE_MESSAGE = "Sessão criada com sucesso!";
+    public static final String VOTE_SUCCESSFULLY = "Voto realizado com sucesso!";
 
 }
